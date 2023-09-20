bc9646ab7be49451af5c35fe48ea6aabca0b49dc
Denis Sderzhko <Xkrudes123@yandex.ru>
2023-09-20 13:07:54 +0300

    complete

---
+++ Practice_1.py
@@ -0,0 +1,30 @@
+class ArrayQueue:
+    def __init__(self):
+        self.array = []
+        self.size = 0
+
+    def is_empty(self):
+        return self.size == 0
+
+    def enqueue(self, data):
+        self.array.append(data)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception("Очередь пуста")
+        return self.array.pop(0)
+
+    def peek(self):
+        if self.is_empty():
+            raise Exception("Очередь пуста")
+        return self.array[0]
+
+    def size(self):
+        return self.size
+
+    def __str__(self):
+        return " -> ".join(map(str, self.array))
+
+
+if __name__ == '__main__':
+    pass
